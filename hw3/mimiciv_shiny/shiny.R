16f62970875f14ce5f44f3d122b291a66fba6845
xingjian xie <xiex6@ucla.edu>
2023-02-28 23:00:12 -0800

    all done

--- hw3/mimiciv_shiny/shiny.R
+++ hw3/mimiciv_shiny/shiny.R
@@ -2,9 +2,9 @@
 
 library(shiny)
 library(ggplot2)
-#runExample("01_hello")
 data = readRDS("hw3/mimiciv_shiny/icu_cohort.rds")
-
+flag_list = c('ethnicity', 'language', 'insurance',
+              'marital_status', 'gender')
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
   
@@ -25,10 +25,15 @@ ui <- fluidPage(
       #             max = 50,
       #             value = 30),
     
-      selectInput(inputId = "variable",
-                  label = "Choose a variable:",
-                  choices = c('Ethnicity', 'Thirdy_Day_Mortality_Rate', 
-                  'Language', 'Insurance', 'Marital_status', 'Gender'))
+      selectInput(inputId = 'variable',
+                  label = 'Choose a variable:',
+                  choices = c('Ethnicity', 'Language', 'Insurance', 
+                  'Marital_status', 'Gender', 'Age_at_Hospital_Admission',
+                  'Bicarbonate', 'Chloride', 'Creatinine', 'Glucose',
+                  'Potassium', 'Sodium', 'Hematocrit', 'White_Blood_Cells',
+                  'Heart_Rate', 'Blood_Pressure_Systolic',
+                  'Blood_Pressure_Mean','Respiratory_Rate',
+                  'Temperature_Fahrenheit'))
                   
     ),
 
@@ -36,53 +41,79 @@ ui <- fluidPage(
     mainPanel(
       
       # Output: Histogram ----
-      plotOutput(outputId = "BarPlot")
+      plotOutput('Plots'),
+      verbatimTextOutput('Summary')
       
     )
   )
 )
 
 
-
-# Define server logic required to draw a histogram ----
 server <- function(input, output) {
-  
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  
   
   var <- reactive({
     switch(input$variable,
            'Ethnicity' = 'ethnicity',
-           'Thirdy_Day_Mortality_Rate' = 'thirty_day_mort',
            'Language' = 'language',
            'Insurance' = 'insurance', 
            'Marital_status' = 'marital_status', 
-           'Gender' = 'gender'
+           'Gender' = 'gender',
+           'Age_at_Hospital_Admission' = 'age_adm',
+           'Bicarbonate' = 'Bicarbonate', 
+           'Chloride' = 'Chloride', 
+           'Creatinine' = 'Creatinine', 
+           'Glucose' = 'Glucose',
+           'Potassium' = 'Potassium',
+           'Sodium' = 'Sodium', 
+           'Hematocrit' = 'Hematocrit',
+           'White_Blood_Cells' = 'White_Blood_Cells',
+           'Heart_Rate' = 'Heart_Rate',
+           'Blood_Pressure_Systolic' = 
+             'Non_Invasive_Blood_Pressure_systolic',
+           'Blood_Pressure_Mean' = 
+             'Non_Invasive_Blood_Pressure_mean',
+           'Respiratory_Rate' = 'Respiratory_Rate',
+           'Temperature_Fahrenheit' = 'Temperature_Fahrenheit'
     )
   })
-  
-  
-  output$BarPlot <- renderPlot({
+
+
+observe(
+
+  output$Plots <- renderPlot({
+  if (var() %in% flag_list){
     
-    ggplot(data, aes_string(x = var())) + geom_bar()
+    ggplot(data, aes_string(x = var())) + geom_bar() +
+      scale_x_discrete(guide = guide_axis(n.dodge=2))
 
-  })
+  } else {
   
+    ggplot(data, aes_string(y = var())) + geom_boxplot() +
+      coord_cartesian(ylim =
+      quantile(data[var()], c(0.05, 0.9), na.rm = T))
+
+  }
+})
+
+)
+
+output$Summary <- renderPrint(
+  if (var() %in% flag_list){
+    
+    table(data[var()])
+  
+  } else {
+    
+    summary(data[var()])
+    
+  }  
+)
+
 }
-
-
 
 shinyApp(ui, server)
 
 
-#ggplot(data, aes_string(x = 'ethnicity')) + geom_bar()
 
 
 
